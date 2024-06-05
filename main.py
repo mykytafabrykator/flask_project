e5410b5fd054867941b3be3fcf86ede48fa59880
mykytafabrykator <71513876+mykytafabrykator@users.noreply.github.com>
2024-06-05 21:37:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,31 @@ import threading
 import time
 from flask import Flask, request, jsonify
 from datetime import datetime
-from dotenv import load_dotenv
 import os
-from waitress import serve
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-# Загрузка переменных окружения
 load_dotenv()
 
-# Конфигурация API из переменных окружения
+# Конфігурація API
 API_URL = os.getenv('API_URL')
 API_TOKEN = os.getenv('API_TOKEN')
 API_HEADERS = {
     'Authorization': f'Bearer {API_TOKEN}'
 }
 
-# Глобальная переменная для хранения данных
+# Глобальна змінна для зберігання даних
 data_cache = None
-last_update_time = None  # переменная для хранения времени последнего обновления
+last_update_time = None  # змінна для зберігання часу останнього оновлення
 
-# Маппинг областей на английском языке
+# Мапінг областей англійською мовою
 regions = [
-    "Crimea", "Volyn", "Vinnytsia", "Dnipro", "Donetsk", 
-    "Zhytomyr", "Zakarpattia", "Zaporizhzhia", "Ivano-Frankivsk", "Kyiv City", 
-    "Kyiv", "Kirovohrad", "Luhansk", "Lviv", "Mykolaiv", 
-    "Odesa", "Poltava", "Rivne", "Sevastopol", "Sumy", 
-    "Ternopil", "Kharkiv", "Kherson", "Khmelnytskyi", 
+    "Crimea", "Volyn", "Vinnytsia", "Dnipro", "Donetsk",
+    "Zhytomyr", "Zakarpattia", "Zaporizhzhia", "Ivano-Frankivsk", "Kyiv City",
+    "Kyiv", "Kirovohrad", "Luhansk", "Lviv", "Mykolaiv",
+    "Odesa", "Poltava", "Rivne", "Sevastopol", "Sumy",
+    "Ternopil", "Kharkiv", "Kherson", "Khmelnytskyi",
     "Cherkasy", "Chernivtsi", "Chernihiv"
 ]
 
@@ -39,7 +37,7 @@ def get_data_from_api():
         response = requests.get(API_URL, headers=API_HEADERS)
         if response.status_code == 200:
             data_cache = response.text
-            last_update_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Обновить время последнего обновления
+            last_update_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Оновити час останнього оновлення
             print(f'[WEB] API request made at {last_update_time}')
         else:
             print(f'Failed to fetch data from API: {response.status_code}')
@@ -50,10 +48,6 @@ def periodic_data_update(interval):
     while True:
         get_data_from_api()
         time.sleep(interval)
-
-@app.route("/")
-def index():
-    return "<h1>Hello!</h1>"
 
 @app.route('/get_value', methods=['GET'])
 def get_value():
@@ -72,17 +66,17 @@ def get_value():
     if device_id <= 0 or device_id > len(data_cache):
         return jsonify({'error': 'Invalid data or ID out of range'}), 400
 
-    # Получить соответствующий символ
+    # Отримати відповідний символ
     value = data_cache[device_id]
     region = regions[device_id - 1]
 
     return jsonify({'value': value, 'obl': region, 'updateTime': last_update_time})
 
 if __name__ == '__main__':
-    # Запуск фонового потока для обновления данных
-    update_interval = 10  # интервал в секундах
+    # Запуск фонового потоку для оновлення даних
+    update_interval = 10  # інтервал у секундах
     data_update_thread = threading.Thread(target=periodic_data_update, args=(update_interval,))
     data_update_thread.daemon = True
     data_update_thread.start()
-    
-    serve(app, host="0.0.0.0", port=80)
+
+    app.run(host='0.0.0.0', port=80)
